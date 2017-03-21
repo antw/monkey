432848245e2fdefbf05896ff0262570e66801800
Anthony Williams <hi@antw.io>
2017-03-21 22:09:00 +0000

    Add evaluation of parsed tokens

--- parser/parser.go
+++ parser/parser.go
@@ -415,7 +415,7 @@ func (p *Parser) peekPrecedence() int {
 }
 
 func (p *Parser) peekError(t token.TokenType) {
-	msg := fmt.Sprintf("expected next token to be %s, got %s intead",
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
 		t, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
 }
